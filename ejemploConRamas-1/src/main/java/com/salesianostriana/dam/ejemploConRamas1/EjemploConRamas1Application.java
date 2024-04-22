b85aa7ee12cb61c9d3d4c842763e6e4f8c0c73c2
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-04-22 21:55:34 +0200

    rama

---
+++ ejemploConRamas-1/src/main/java/com/salesianostriana/dam/ejemploConRamas1/EjemploConRamas1Application.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejemploConRamas1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploConRamas1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploConRamas1Application.class, args);
+	}
+
+}
